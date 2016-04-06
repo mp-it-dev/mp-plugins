8422461aad64b69e2e1c5051e1f07fd825a5b7fa
hetianqi <449165578@qq.com>
2016-04-06 10:13:04 +0800

    change dir

---
+++ assets/js/config.js
@@ -0,0 +1,26 @@
+require.config({
+    baseUrl: './assets/js',
+    paths: {
+        'jquery': 'lib/jquery-1.11.3.min',
+        'bootstrap': 'lib/bootstrap.min',
+        'uploadify': 'lib/jquery.uploadify',
+        'tlayer': 'lib/jquery.tlayer',
+        'plugins': 'lib/jquery.plugins',
+        'util': 'lib/util'
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery']
+        },
+        'uploadify': {
+            deps: ['jquery']
+        },
+        'tlayer': {
+            deps: ['jquery']
+        },
+        'plugins': {
+            deps: ['jquery']
+        }
+    },
+    urlArgs: 'bust=' + (new Date()).getTime()   //开发环境下禁用缓存，生产环境要移除
+});
