8c301528a85834c172fc14b03063c8c7dde9de9d
hetianqi <449165578@qq.com>
2016-11-23 17:06:18 +0800

    1、增加单职位选择和多职位选择 2、优化选择器样式

--- assets/plugins/selector/selector.js
+++ assets/plugins/selector/selector.js
@@ -87,6 +87,40 @@
 				}
 			});
 		},
+		//单职位
+		singleJob: function (option) {
+			window.singleJobCallback = option.callback;
+			var t = new Date().getTime();
+
+			$.content({
+				layerID: option.layerID || false,
+				theme: 'blue',
+				header: '单职位',				
+				content: {
+					width: 400,
+					height: 480,
+					padding: 0,
+					src: this.baseUrl + 'singleJob.html?callback=singleJobCallback&apiurl=' + encodeURIComponent(this.apiUrl) + '&t=' + t
+				}
+			});
+		},
+		//多职位
+		multiJob: function (option) {
+			window.multiJobCallback = option.callback;
+			var t = new Date().getTime();
+
+			$.content({
+				layerID: option.layerID || false,
+				theme: 'blue',
+				header: '多职位',
+				content: {
+					width: 630,
+					height: 480,
+					padding: 0,
+					src: this.baseUrl + 'multiJob.html?callback=multiJobCallback&apiurl=' + encodeURIComponent(option.apiUrl || this.apiUrl) + '&t=' + t
+				}
+			});
+		},
 		//单部门职位
 		singleDepJob: function (option) {
 			window.singleDepJobCallback = option.callback;
